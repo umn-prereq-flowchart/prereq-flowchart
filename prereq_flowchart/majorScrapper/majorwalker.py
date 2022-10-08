68da318fcafa39ddb401e4912e5b2b280110273b
JQM <hgrpoiu@gmail.com>
2022-10-08 18:34:57 -0500

    majorwalker now scrapes class description and all classes

--- prereq_flowchart/majorScrapper/majorwalker.py
+++ prereq_flowchart/majorScrapper/majorwalker.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import majorinfo
 import json
+import re
 
 browser=webdriver.Firefox()
 browser.maximize_window()
@@ -10,12 +11,17 @@ linkFile=open("majorLinks.json","r")
 majorLinks=json.loads(linkFile.read())
 
 
-for i in range(len(majorLinks)):
+for i in range(3):
     browser.get(majorLinks[i])
     print(browser.find_element(By.CLASS_NAME,"programtitle").get_attribute("textContent"))
 
-    credElem=browser.find_element(By.CLASS_NAME,'catalogList')
-    classElems=browser.find_elements(By.CLASS_NAME,"catText")
-    print(classElems[i].get_attribute("textContent").strip())
+    classes=browser.find_elements(By.XPATH,"//a[@onclick]/..")
+    for j in range(len(classes)):
+        attr=str(classes[j].find_element(By.TAG_NAME,"a").get_attribute("onclick"))
+        classID=re.search("courseInfo_[0-9]*",attr)[0]
+        print(classes[j].get_attribute("textContent").split())
+        print(browser.find_element(By.ID,classID).find_element(By.CLASS_NAME,"textField").get_attribute("textContent"))
 
+
+browser.close()
 
