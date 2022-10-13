d188cd1da8c493f14fe9e46e91b365850d105432
Kanishk-K <kanishkkacholia@gmail.com>
2022-10-12 19:06:10 -0500

    JSON based graph generation established.

--- prereq_flowchart/TEMP_graphviz/test.py
+++ prereq_flowchart/TEMP_graphviz/test.py
@@ -1,4 +1,6 @@
+from typing import Dict, List, Union, cast
 import graphviz
+import json
 from os.path import join, dirname, exists
 from os import mkdir
 
@@ -7,9 +9,33 @@ IMAGEDIR = join(
 )  # /../../../out aka prereq-flowchart/out/
 
 
+def read_data(filename: str) -> List[Dict[str, Union[str, List[str]]]]:
+    with open(filename, "r") as f:
+        return cast(List[Dict[str, Union[str, List[str]]]], json.load(f))
+
+
 def simple_graph() -> graphviz.Digraph:
-    retGraph = graphviz.Digraph("wide")
+    retGraph = graphviz.Digraph(
+        "wide",
+        node_attr={"color": "darkgoldenrod1", "style": "filled"},
+    )
     retGraph.edges(("0", str(i)) for i in range(1, 10))
+    return retGraph
+
+
+def data_graph(data: List[Dict[str, Union[str, List[str]]]]) -> graphviz.Digraph:
+    retGraph = graphviz.Digraph(
+        "wide",
+        node_attr={
+            "color": "darkgoldenrod1",
+            "style": "filled",
+            "shape": "invhouse",
+            "fontname": "Century Gothic Bold",
+        },
+    )
+    for node in data:
+        for prereq in node["Prereqs"]:
+            retGraph.edge(prereq, node["Classname"])
     return retGraph
 
 
@@ -17,4 +43,6 @@ if __name__ == "__main__":
     print("Test Successful")
     if not exists(IMAGEDIR):
         mkdir(IMAGEDIR)
-    simple_graph().render(join(IMAGEDIR, "Graph1"))
+    data_graph(read_data(join(dirname(__file__), "dummy_data.json"))).render(
+        join(IMAGEDIR, "Graph1")
+    )
