de4218e9b773c326d8b65bb2e63b5202465a469a
doggu <someoneyoumayknow0@gmail.com`>
2022-12-08 05:10:12 -0600

    cleanup

--- prereq_flowchart/compose.py
+++ prereq_flowchart/compose.py
@@ -9,21 +9,21 @@ non_existent = []
 buf = []
 
 
-def get_courses(course: CourseNumber, n=0) -> [CourseNumber]:
+def get_courses(course: CourseNumber, n=0):  # -> [CourseNumber]:
     """
     returns a list of classes (not a hierarchy!) that are a prerequisite to the course provided.
     Args:
-        course: 
-        n: 
+        course:
+        n:
 
     Returns:
 
     """
-    # print(course)
     global buf
-    if n > 5:
-        # print(f"bottomed out: {course}")
-        return []
+    # depth currently only necessary for marking first level to clear buf appropriately
+    # if n > 5:
+    #     print(f"bottomed out: {course}")
+    #     return []
 
     if course not in c:
         return []
@@ -36,7 +36,7 @@ def get_courses(course: CourseNumber, n=0) -> [CourseNumber]:
             continue
         if prereq.catalog_number in c:
             prereqs.append(prereq.catalog_number)
-            prereqs.extend(get_courses(course=prereq.catalog_number, n=n+1))
+            prereqs.extend(get_courses(course=prereq.catalog_number, n=n + 1))
         else:
             if prereq.catalog_number not in non_existent:
                 print(f"warning: prereq does not exist: {prereq.catalog_number}")
@@ -58,11 +58,9 @@ def make_json_data_for_majors():
     major_table = {}
 
     for major in m:
-        # print(major.name)
         courses = []
         warnings = ""
         for course in major.courses:
-            # print(course.catalogTitle)
             num = course.catalogTitle.split(" ")
             if len(num) != 2:
                 warnings += f"\tinvalid course: {course.catalogTitle}\n"
@@ -76,27 +74,22 @@ def make_json_data_for_majors():
                 for pr in get_courses(num):
                     if c[pr] not in courses:
                         courses.append(c[pr])
-            # courses.extend(get_courses(c[num]))
         if warnings != "":
             print(f"{major.name}\n", warnings, end="")
 
         # i = 0
-        # while i in range(0, len(courses)):
+        # while i < len(courses):
         #     if "H" in courses[i].catalog_number.number:
         #         courses.pop(i)
         #     else:
         #         i += 1
 
         graph_data = []
-        # todo: recursively search for prerequisites to prerequisites
+        # instead of searching recursively here, recursion is done while gathering major courses. the only job of this loop is to create edges which can be done just with Course objects due to their prerequisites attribute. if all classes are touched recursively, then they will all list their prerequisites and the graph generator will compose the edges together properly
         for course in courses:
-            # number = (
-            #     course.catalog_number.department + " " + course.catalog_number.number
-            # )
             number = course.catalog_number.to_str()
             prereqs = []
             for p in course.prerequisites:
-                # name = p.catalog_number.department + " " + p.catalog_number.number
                 name = p.catalog_number.to_str()
                 if name not in prereqs:
                     prereqs.append(name)
